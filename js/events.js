d3853cd7bf442d575c1e548376c65565232c557a
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2020-10-07 23:09:09 +0200

    Fix the bars overlapping the controls

--- js/events.js
+++ js/events.js
@@ -1,3 +1,17 @@
+const controls = document.getElementById('controls');
+
+function getViewportHeight() {
+  return Math.max(document.documentElement.clientHeight || 0,
+    window.innerHeight || 0);
+}
+
+function setViewHeight() {
+  view.style.height = (getViewportHeight() - controls.offsetHeight) + 'px';
+}
+
+window.addEventListener('load', setViewHeight);
+window.addEventListener('resize', setViewHeight);
+
 visualizer = new Visualizer();
 
 shuffleButton.addEventListener('click', function() {
